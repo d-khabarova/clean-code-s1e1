9e3700ac9e0033077c8f40396b0ef3cfdd487824
Darya Petina <darya.khabarova@gmail.com>
2022-05-29 14:01:14 +0300

    refactor: change tag selectors rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className="todo-list__item";
+    checkBox.className="todo-list__checkbox";
+
     label.innerText=taskString;
     label.className='todo-list__label-task';
 
@@ -44,6 +47,7 @@ var createNewTaskElement=function(taskString){
     editButton.className="todo-list__button todo-list__button_edit";
 
     deleteButton.className="todo-list__button todo-list__button_delete";
+    deleteButtonImg.className='img-remove';
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -85,11 +89,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".todo-list__button_edit");
-    var containsClass=listItem.classList.contains("edit-mode");
-    //If class of the parent is .edit-mode
+    var containsClass=listItem.classList.contains("todo-list__item_edit-mode");
+    //If class of the parent is .todo-list__item_edit-mode
     if(containsClass){
 
-        //switch to .edit-mode
+        //switch to .todo-list__item_edit-mode
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +102,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    //toggle .todo-list__item_edit-mode on the parent.
+    listItem.classList.toggle("todo-list__item_edit-mode");
 };
 
 
